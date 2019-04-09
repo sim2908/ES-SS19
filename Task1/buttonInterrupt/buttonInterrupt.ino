a1425d6c30155e45e110eff55d53f79ffc9d2740
Alexander Herms <sitlsimon2908@gmail.com>
2019-04-09 11:57:38 +0200

    interrupt

--- Task1/buttonInterrupt/buttonInterrupt.ino
+++ Task1/buttonInterrupt/buttonInterrupt.ino
@@ -1,11 +1,22 @@
 //TODO A-1.4 Button with interrupt
 
+const uint8_t led_pin = 7;
+const uint8_t button_pin = 3;
+
+volatile int led_state = LOW;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(led_pin, OUTPUT);
+  pinMode(button_pin, INPUT);
+
+  attachInterrupt(button_pin, blink_led, RISING);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(led_pin, led_state);
+}
 
+void blink_led(){
+  led_state = ! led_state;
 }
